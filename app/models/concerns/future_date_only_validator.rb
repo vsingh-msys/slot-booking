865038d9d6013a7da2c3ba7c7aee0015d669747f
Vivek Singh <kvivek1115@gmail.com>
2017-10-08 14:32:17 +0530

    Passed day validation remove today

--- app/models/concerns/future_date_only_validator.rb
+++ app/models/concerns/future_date_only_validator.rb
@@ -1,6 +1,6 @@
 class FutureDateOnlyValidator < ActiveModel::EachValidator
 	def validate_each(record, attribute, value)
-	    if value.present? && value <= Date.today
+	    if value.present? && value < Date.today
 	      record.errors.add attribute,
 	                        (options[:message] || "can't be in the past")
 	    end
